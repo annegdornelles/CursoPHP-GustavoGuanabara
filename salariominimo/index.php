440a26b1ddf7231cf95bdc5f62a9dd648c54fd74
Anne G. Rufino Dornelles <gabrielleannerd@gmail.com>
2024-05-07 15:05:46 -0300

    Create index.php

---
+++ salariominimo/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quantos salários mínimos seu sálario equivale?</title>
+</head>
+<body>
+    <?php
+       
+
+
+    ?>
+    <h1>Quantos salários mínimos seu sálario equivale?</h1>
+
+    <label for="salario">Insira seu salário:</label>
+    <input type="number" id="salario" name="salario"><br><br>
+
+    <input type="submit" value="Calcular">
+
+    <h2>Resultaos:</h2>
+
+    <?php
+
+    ?>
+    
+</body>
+</html>
