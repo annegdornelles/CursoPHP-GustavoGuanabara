f1d4b01fbc5c9bc2419e4e6766fffb2c28315ff3
Anne G. Rufino Dornelles <gabrielleannerd@gmail.com>
2024-05-07 14:39:46 -0300

    Create index.php

---
+++ analisedeumadivisao/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anatomia de uma divisão</title>
+</head>
+<body>
+
+    <?php
+
+       $divid=$_POST["divid"] ?? 0;
+       $divis=$_POST["divis"] ?? 1;
+
+    ?>
+    <h1>Anatomia de uma divisão</h1>
+
+    <form action="" method="POST">
+
+    <label for="divid">Dividendo</label><input type="number" name="divid" id="divid" value="<?=$divid?>" min="0"><br><br>
+    <label for="divis">Divisor</label><input type="number" name="divis" id="divis" value="<?=$divis?>" min="1"><br><br>
+     
+    <input type="submit" value="Analisar">
+
+    </form>
+
+    <h2>Estrutura da divisão:</h2>
+
+    <?php
+
+       $quo=intdiv($divid,$divis);
+       $resto=$divid%$divis;
+
+       echo "<ul>";
+       echo"<li>Dividendo:$divid</li><li>Divisor:$divis</li><li>Quociente:$quo</li><li>Resto:$resto</li>";
+
+       echo"</ul>";
+
+    ?>
+</body>
+</html>
